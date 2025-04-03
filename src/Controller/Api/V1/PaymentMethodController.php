aee619b970f8ccc15fecd20e1ad09d7fc81a8940
khaled abodaif <khaled.abodaif@yahoo.com>
2025-04-03 22:30:50 +0200

    payment available methods endpoint

---
+++ src/Controller/Api/V1/PaymentMethodController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller\Api\V1;
+
+use App\Enum\PaymentMethodEnum;
+use App\Helper\ApiResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controller for handling payment method related API endpoints.
+ */
+#[Route('/api/v1/payment/methods', name: 'api_v1_payment_methods')]
+class PaymentMethodController extends AbstractController
+{
+
+    /**
+     * Lists all available payment methods.
+     *
+     * @return Response The JSON response containing the list of payment methods.
+     */
+    #[Route('', name: 'index')]
+    public function list(): Response
+    {
+        return ApiResponse::data(PaymentMethodEnum::toArray());
+    }
+
+}
