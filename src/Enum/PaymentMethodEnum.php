5d3ef420d82bc34056fd316a496b447604cb25cc
khaled abodaif <khaled.abodaif@yahoo.com>
2025-04-04 05:34:28 +0200

    enum docs for class convention

--- src/Enum/PaymentMethodEnum.php
+++ src/Enum/PaymentMethodEnum.php
@@ -4,6 +4,13 @@ namespace App\Enum;
 
 enum PaymentMethodEnum : string
 {
+    /*
+     * the enum name reflect to payment class name
+     * for example ACI the class is AciPaymentService
+     * for example PAYMENT_NAME the class is PaymentNamePaymentService
+     *
+     * */
+
     case SHIFT4 = 'shift4';
     case ACI = 'aci';
     // Add more as needed each case should reflect a class
