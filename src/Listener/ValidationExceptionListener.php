caef7614512792bd3c5037569724474d60b8c5d2
khaled abodaif <khaled.abodaif@yahoo.com>
2025-04-05 09:08:21 +0200

    create validation exception listener

---
+++ src/Listener/ValidationExceptionListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Listener;
+
+use App\Helper\ApiResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+
+class ValidationExceptionListener
+{
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $throwable = $event->getThrowable();
+        $validationException = null;
+        $errors = [];
+
+        if ($throwable instanceof ValidationFailedException) {
+            $validationException = $throwable;
+        } elseif ($throwable->getPrevious() instanceof ValidationFailedException) {
+            $validationException = $throwable->getPrevious();
+        } elseif ($throwable->getPrevious() instanceof \ValueError && str_contains($throwable->getMessage(), 'is not a valid backing value')) {
+            $errors['parameter'] = "Please Enter a valid data";
+            $event->setResponse(ApiResponse::error($errors, "Validation Error", 422));
+        }
+
+        if ($validationException) {
+            foreach ($validationException->getViolations() as $violation) {
+                $errors[$violation->getPropertyPath()] = $violation->getMessage();
+            }
+
+
+            $event->setResponse(ApiResponse::error($errors, "Validation Error", 422));
+        }
+
+    }
+
+}
