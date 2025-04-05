60e938840a23f2e9d57e09fa06bbc323a1b14f1b
khaled abodaif <khaled.abodaif@yahoo.com>
2025-04-05 09:04:30 +0200

    create http helper

---
+++ src/Dto/HttpResponseDto.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Dto;
+
+class HttpResponseDto
+{
+
+    private bool $status = true;
+    private int $code = 200;
+    private string $message = "Success!";
+    private array $response = [];
+
+    public function toArray() : array
+    {
+        return [
+            'status' => $this->status,
+            'code' => $this->code,
+            'message' => $this->message,
+            'response' => $this->response,
+        ];
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function getCode(): int
+    {
+        return $this->message;
+    }
+
+    public function getResponse(): array
+    {
+        return $this->response;
+    }
+
+    public function setStatus(bool $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+    public function setResponse(array $response): void
+    {
+        $this->response = $response;
+    }
+
+    public function setCode(int $code): void
+    {
+        $this->code = $code;
+    }
+}
---
+++ src/Helper/HttpHelper.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Helper;
+
+use App\Dto\HttpResponseDto;
+use Symfony\Component\HttpClient\HttpClient;
+
+class HttpHelper
+{
+
+    private string $url;
+    private array $payload;
+    private array $headers;
+
+    private HttpResponseDto $httpResponse;
+
+    private function __construct()
+    {
+        $this->httpResponse = new HttpResponseDto();
+    }
+
+    public static function make(): self
+    {
+        return new self();
+    }
+
+    public function post(): self
+    {
+
+        try {
+            $httpClient = HttpClient::create();
+
+            $this->httpResponse->setResponse($httpClient->request('POST', $this->url, [
+                'json' => $this->payload,
+                'headers' => $this->headers
+            ])->toArray());
+
+        } catch (\Exception $e) {
+
+            $this->httpResponse->setStatus(false);
+            $this->httpResponse->setMessage($e->getMessage());
+            $this->httpResponse->setCode($e->getCode());
+
+        }
+
+        return $this;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function setPayload(array $payload): self
+    {
+        $this->payload = $payload;
+        return $this;
+    }
+
+    public function setHeaders(array $headers): self
+    {
+        $this->headers = $headers;
+        return $this;
+    }
+
+    public function getResponse(): HttpResponseDto
+    {
+        return $this->httpResponse;
+    }
+}
