fd83ae67b9bd79d9988cc83e7bf577370bee615d
khaled abodaif <khaled.abodaif@yahoo.com>
2025-04-05 09:01:33 +0200

    enhance code quality

--- src/Controller/Api/V1/PaymentController.php
+++ src/Controller/Api/V1/PaymentController.php
@@ -3,41 +3,44 @@
 namespace App\Controller\Api\V1;
 
 use App\Dto\PaymentDto;
+use App\Enum\PaymentMethodEnum;
 use App\Helper\ApiResponse;
+use App\Service\PaymentFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api/v1/payments', name: 'api_v1_payments')]
 class PaymentController extends AbstractController
 {
 
     public function __construct(
-        private readonly SerializerInterface $serializer ,
-        private readonly ValidatorInterface $validator
+        private readonly SerializerInterface $serializer,
+        private readonly PaymentFactory      $paymentFactory
     )
     {
     }
 
     #[Route('/{method}', name: 'payments', methods: ['POST'])]
-    public function store(string $method , Request $request ):JsonResponse
+    public function store(PaymentMethodEnum $method, #[MapRequestPayload] PaymentDto $paymentDto): JsonResponse
     {
-        // map to dto and Validate payment request
-        $paymentDto = $this->serializer->deserialize($request->getContent(), PaymentDto::class, 'json',['groups' => ['write']]);
         $paymentDto->setMethod($method);
 
-        // validate
-        $errors = $this->validator->validate($paymentDto);
-        if ($errors->count() > 0) {
-            return ApiResponse::error($errors);
+        try {
+
+            $paymentResponse = $this->paymentFactory->get($method)->init($paymentDto)->pay();
+
+            $data = $this->serializer->normalize($paymentResponse, null, ['groups' => ['read']]);
+            return ApiResponse::data($data, "Payment processed successfully");
+
+        } catch (\Throwable $e) {
+
+            return ApiResponse::error([], "Payment failed !", Response::HTTP_FAILED_DEPENDENCY);
         }
 
-        // map to payment response dto
-        $data = $this->serializer->serialize($paymentDto, 'json', ['groups' => ['read']]);
-        return ApiResponse::data($data,"Payment processed successfully");
 
     }
 }
